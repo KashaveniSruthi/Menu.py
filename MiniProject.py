2e5f53df9af143e070fc22152b12fd3f9ef9f24e
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2024-10-28 11:36:03 +0530

    Add files via upload

---
+++ MiniProject.py
@@ -0,0 +1,37 @@
+#define menucard
+menu={
+    'Pizza':60,
+    "Pasta":50,
+    "Burger":80,
+    "Salad":70,
+    "Coffee":40,
+}
+
+#greeting
+print("Welcome to our Restaurant")
+
+print("Pizza: Rs.60\nPasta: Rs.50\nBurger: Rs.80\nSalad: Rs.70\nCoffee: Rs.40")
+
+order_total=0
+
+item_1=input("Enter the name of item you want to order = ")
+if item_1 in menu:
+    order_total+= menu[item_1]
+    print(f"Your item {item_1} has been added to your order")
+
+else:
+    print(f"Ordered item {item_1} is not available yet!")
+
+another_order=input("Do you want to add another item? (Yes/No)")
+if another_order=="Yes":
+    item_2=input("Enter the name of second item = ")
+    if item_2 in menu:
+        order_total+=menu[item_2]
+        print(f"Ordered item {item_2} has been added to your order")
+    else:
+        print(f"Ordered item {item_2} is not available!")
+
+print(f"The total amount of items to pay is {order_total}")
+print("  ***** Thank you *****  ")
+
+
